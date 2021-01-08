b842a396a3cb3665b4b36cf2f126073513f8adcc
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-01-07 20:22:52 -0800

    Update

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const { getRecords, pool } = require("./db.js");
 getRecords("select * from widgets")
   .then(res => {
     console.log(res.rows);
-    return getRecords("select * from wwwwidgets");
+    return getRecords("select * from users limit 3");
+  })
+  .then(res => {
+    console.log(res.rows);
+    return getRecords("select * from wwidgets");
   })
   .catch(err => {
-    console.log("catch:", Object.keys(err));
+    // console.log("catch:", Object.keys(err));
     console.log("catch:", err.detail);  // Not all of these are useful
-    console.log("catch:", err.code);
+    console.log("catch:", err);
   })
   .finally(() => {
     console.log("finally");
