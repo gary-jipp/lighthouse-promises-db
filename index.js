cd5ade6bdf607b2a92dab6530bf5b587a009186f
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-01-07 19:50:02 -0800

    Update

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 console.log("\n***Start of my Code ***");
 const { getWidgets, pool } = require("./db.js");
 
+// Call the promise in a function
+const myFunction = function () {
+  const promise = getWidgets();
+  promise.then(res => {
+    console.log(res.rows);
+  });
 
-// call a function that returns a Promise
-const promise = getWidgets();
-console.log(promise);
-
-// Need to know when the promise is "done" (resolved)
-// This is the basic promise pattern
-promise.then();
-
-// then() needs a callback function or we'll never know what happened
-const callback = function (res) {
-  console.log(res.rows);
+  // return res;    // oops, can't do this
+  return promise;   //  return the promise
 };
-promise.then(callback);
 
-// Normally we would write the callback inline, using an arrow function
-promise.then(res => console.log(res.rows));
+// Call my new Function
+const result = myFunction();
+console.log(result);    // Now my new function returns a promise
 
-// This just closes the pool quicker
+// Now I have to "then" the result from my function also
+result.then(res => console.log(res.rows));
+
+// You can "then" the same promise over and over again.
+result.then(res => console.log(res.rows));
+result.then(res => console.log(res.rows));
+result.then(res => console.log(res.rows));
+
+
 pool.end();
-
 console.log("***End of my Code ***\n");
