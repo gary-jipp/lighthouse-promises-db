612890dc155eec04dd58e46f28cef14ccad7f739
Gary J <github@accu.ca>
2021-07-19 15:37:51 -0700

    renamed main

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 console.log("\n***Start of my Code ***");
-const { getWidgets, pool } = require("./db.js");
+const { getUsers, pool } = require("./db.js");
 
 // Call my new Function
-const result = getWidgets();
+const result = getUsers();
 console.log(result);    // Now my new function returns a promise
 
 // Now I have to "then" the result from my function also
 result.then(res => console.log(res.rows));
 
 // You can "then" the same promise over and over again.
-result.then(res => console.log(res.rows));
-result.then(res => console.log(res.rows));
-result.then(res => console.log(res.rows));
+// result.then(res => console.log(res.rows));
+// result.then(res => console.log(res.rows));
+// result.then(res => console.log(res.rows));
 
 
 pool.end();
