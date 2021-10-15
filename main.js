f54b46ca5615315f726b314303cbc4cf18bf3de5
Gary J <github@accu.ca>
2021-10-14 22:27:14 -0700

    Update

--- main.js
+++ main.js
@@ -1,33 +1,18 @@
+console.log("***Start of my Code ***\n");
+
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: "vagrant",
-  password: "123",
+  user: "labber",
+  password: "labber",
   host: "localhost",
   database: "lightbnb",
 });
 
-// call a function that returns a Promise
-const query = "SELECT id, name FROM users LIMIT 5";
-const promise = pool.query(query);
-console.log(promise);
+const sql = "select id, name, email from users limit 10"; 
+pool.query(sql)
+  .then(res => {
+    console.log(res.rows);
+  });
 
-// Need to know when the promise is "done" (resolved)
-// This is the basic promise pattern
-promise.then();
-
-// then() needs a callback function or we'll never know what happened
-const callback = function (res) {
-  console.log();
-  console.log(res.rows);
-};
-promise.then(callback);
-console.log();
-
-// Notice we can "then" the same promise more than once
-
-// Normally we would write the callback inline, using an arrow function
-promise.then(res => {
-  console.log(res.rows);
-});
 
 console.log("***End of my Code ***\n");
