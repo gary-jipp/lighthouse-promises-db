41d2876c9aac8d836341236f830c6e5aa610c3e4
Gary J <github@accu.ca>
2022-02-14 14:51:54 -0800

    Save

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # Promises
 
-- [ ] basic Promise pattern
-- [ ] calling then() the same Promise
-- [ ] chaining then()
-- [ ] return from then
-- [ ] Promise Functions
-- [ ] using catch()
-- [ ] return from catch()
-
-
+![Components Before](resources/promises.jpg)
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("\n***Start of my Code ***");
-
 const { Pool } = require("pg");
 const pool = new Pool({
   user: "labber",
@@ -8,30 +6,8 @@ const pool = new Pool({
   database: "midterm",
 });
 
-// call a function that returns a Promise
-const query = "SELECT id, name FROM users ";
-const promise = pool.query(query);
-console.log(promise);
-
-// Need to know when the promise is "done" (resolved)
-// This is the basic promise pattern
-promise.then();
-
-// then() needs a callback function or we'll never know what happened
-const callback = function(res) {
-  console.log();
-  console.log(res.rows);
-};
-
-promise.then(callback);
-
-// Notice we can "then" the same promise more than once
-promise.then(callback);
-promise.then(callback);
-
-// Normally we would write the callback inline, using an arrow function
-promise.then(res => {
-  console.log(res.rows);
-});
-
-console.log("***End of my Code ***\n");
+const sql = "select * from users";
+pool.query(sql)
+  .then(res => {
+    console.log(res.rows);
+  });
