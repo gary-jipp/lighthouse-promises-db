b52b83cbcdd3d5f94f560a4c53271b044f2bba32
Gary J <github@accu.ca>
2022-05-06 10:39:46 -0700

    update

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const axios = require("axios");
 
 console.log("\n***Start of my Code ***");
 
-const url = "https://api.kanye.rest/";
+// const url = "https://api.kanye.rest";
+const url = "https://officeapi.dev/api/quotes/random";
 const promise = axios.get(url);
 
 promise
   .then(result => {
     console.log(result.data.quote);
+    console.log(result.data.data.content);
   })
   .catch(e => console.log(e.message));
 
