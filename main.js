b7b3b047e5a97999a06a79f7daa3aa7b70544cf8
gary <github@accu.ca>
2022-11-20 21:37:08 -0800

    Update

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-const axios = require('axios');
-
-const url = "http://api.office.rest";
-
-axios.get(url)
-  .then(res => {
-    console.log(res.data);
-  })
-  .catch(err => {
-    console.log(err.message);
-  })
-  ;
-
-
-console.log("*** End of our Code ** ");
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "midterm",
+});
